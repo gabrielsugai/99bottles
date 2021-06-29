7e8938a93236fae617c4d65c5721f3bd63292e23
Gabriel Sugai <gabrielsugai@gmail.com>
2021-06-29 11:53:14 -0300

    Primeira refatoracao do metodo lyrics

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,29 +1,24 @@
 class BottleVerse
   def self.lyrics(count)
-    if count == 6
-      "1 six-pack of beer on the wall, 1 six-pack of beer.\n" +
-      if (count-1) == 6
-        "Take one down and pass it around, 1 six-pack of beer on the wall.\n"
-      elsif (count-1) == 1
-        "Take one down and pass it around, #{count - 1} bottle of beer on the wall.\n"
-      else
-        "Take one down and pass it around, #{count - 1} bottles of beer on the wall.\n"
-      end
-    elsif count == 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif count == 0
+    case count
+    when 0
       "No more bottles of beer on the wall, no more bottles of beer.\n" +
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 6
+      "1 six-pack of beer on the wall, 1 six-pack of beer.\n" +
+      "Take one down and pass it around, #{count - 1} bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 7
+      "#{count} bottles of beer on the wall, #{count} bottles of beer.\n" +
+      "Take one down and pass it around, 1 six-pack of beer on the wall.\n"
+    when 2
+      "#{count} bottles of beer on the wall, #{count} bottles of beer.\n" +
+      "Take one down and pass it around, #{count - 1} bottle of beer on the wall.\n"
     else
       "#{count} bottles of beer on the wall, #{count} bottles of beer.\n" +
-      if (count-1) == 6
-        "Take one down and pass it around, 1 six-pack of beer on the wall.\n"
-      elsif (count-1) == 1
-        "Take one down and pass it around, #{count - 1} bottle of beer on the wall.\n"
-      else
-        "Take one down and pass it around, #{count - 1} bottles of beer on the wall.\n"
-      end
+      "Take one down and pass it around, #{count - 1} bottles of beer on the wall.\n"
     end
   end
 end
